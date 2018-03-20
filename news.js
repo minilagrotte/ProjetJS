4e989aea8a915e50b0104fae5e18d30683c6ffab
LASCAUX Romain <lascauxr@hostname.iut2.upmf-grenoble.fr>
2018-03-20 14:25:09 +0100

    tesst

--- news.js
+++ news.js
@@ -4,30 +4,41 @@ var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titr
 
 function ajouter_recherche()
 {
+
 	var elem = document.getElementById("zone_saisie");
 	var recherche = document.getElementById("recherches-stockees");
 	var paragraphe = document.createElement('p');
 	paragraphe.class = 'titre-recherche';
+
 	var label = document.createElement('label');
 	label.innerText = elem.value;
+	label.setAttribute('onclick','selectionner_recherche(this)');
+
 	var im = document.createElement('img');
 	im.src = "croix30.jpg";
 	im.class = 'icone-croix';
+	im.setAttribute('onclick','supprimer_recherche(this)');
+
 	paragraphe.appendChild(label);
 	paragraphe.appendChild(im);
 	recherche.appendChild(paragraphe);
+
+	recherches.push(label.innerText);
 }
 
 function supprimer_recherche(e)
 {
-
-
+	var parent = e.parentElement;
+	var i = recherches.indexOf(parent.firstChild.innerText);
+	recherches.splice(i,1);
+	var elem = document.getElementById("recherches-stockees");
+	elem.removeChild(parent);
 }
 
 
 function selectionner_recherche(e)
 {
-
+	console.log(e);
 }
 
 
