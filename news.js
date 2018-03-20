07f0701a6e7fefaec9065f037da011bdcb874ba6
LAURENT Thomas <thomas.laurent38@gmail.com>
2018-03-20 14:30:41 +0100

    Cookies

--- news.js
+++ news.js
@@ -16,6 +16,8 @@ function ajouter_recherche()
 	paragraphe.appendChild(label);
 	paragraphe.appendChild(im);
 	recherche.appendChild(paragraphe);
+	console.log(paragraphe.value);
+	nouveauCookie('recherches',recherches.value,1000);
 }
 
 function supprimer_recherche(e)
@@ -33,7 +35,23 @@ function selectionner_recherche(e)
 
 function init()
 {
+	let recherches = [];
+	recherches = document.cookie.split("; ");
+	console.log("Liste de vos cookies actuels : "+recherches);
 
+	let boutonsLibres = document.getElementsByClassName("bouton_simple bouton_libre");
+
+	for(let c = 0; c < recherches.length; c++) {
+			let cookieCourant = " ";
+			cookieCourant = recherches[c].split("=");
+
+			for (let b = 0; b < boutonsLibres.length; b++) {
+				if (cookieCourant[0] == boutonsLibres[b].id){
+					boutonsLibres[b].value = cookieCourant[1];
+					boutonsLibres[b].onclick = cookieCourant[1];
+				}
+			}
+	}
 }
 
 
@@ -60,4 +78,18 @@ function sauver_nouvelle(e)
 function supprimer_nouvelle(e)
 {
 
+}
+
+//////////Nouvelles Fonctions//////////
+
+function nouveauCookie(nom,valeur,jours) {
+		if (jours != 0) { // Si le nombre de jours est renseigné
+			let date = new Date(); // Création d'un nouvel objet Date
+			let dateExpiration = date.getTime()+(jours*24*60*60*1000); // Création date expiration
+			date.setTime(dateExpiration);
+			let valeurExpiration = "; expires="+date.toGMTString();
+		} else { // Si le nombre de jours n'est pas renseigné
+			let valeurExpiration = ""; // valeurExpiration est nulle
+		}
+		document.cookie = nom+"="+valeur+valeurExpiration+"; path=/";
 }
